aad05e2f8e39a84331b69807e6f863afad3d7ce3
Rongfei Pan <rongfei@kth.se>
2024-03-29 22:48:28 +0800

    ⚡️ Player moving motion updated
    player can accelerate smoothly, multiple keys won't cause conflicts now, setup maximum speed.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,81 +1,54 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    public float m_speed = 10f;
+    // public float m_speed = 10f;
     private Vector3 targetDirection;
+    private float speed, acceleration = 20f;
+
     // Update is called once per frame
     void Update()
     {
-        MoveByKey(KeyCode.UpArrow);
-        MoveByKey(KeyCode.DownArrow);
-        MoveByKey(KeyCode.LeftArrow);
-        MoveByKey(KeyCode.RightArrow);
+        MoveByKey();
     }
 
-    private void MoveToCursor()
+    private void MoveByKey()
     {
+        if (Input.GetKey(KeyCode.D)) targetDirection = transform.right;
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)) targetDirection = -transform.right;
 
-        Ray lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
 
-        if (Physics.Raycast(lastRay, out hit))
+        if (GetDirectionKey())
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
-
-    }
-
-
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-
-
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
-    }
-
-    private void MoveByKey(KeyCode key)
-    {
-        // if (Input.GetKeyDown(key))
-        // {
-        //     if (key == KeyCode.DownArrow)
-        //     {
-
-        //     }
-        // }
-
-
-        if (Input.GetKey(key))
-        {
-            if (key == KeyCode.DownArrow)
-            {
-                targetDirection = -transform.right;
-            }
-            else if (key == KeyCode.LeftArrow)
-            {
-                targetDirection = -transform.right;
-            }
-            else if (key == KeyCode.RightArrow)
-            {
-                targetDirection = transform.right;
-            }
-
             if (targetDirection == null)
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.forward), 2f * Time.deltaTime);
-            else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), 2f * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.forward), 3f * Time.deltaTime);
+            else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), 3f * Time.deltaTime);
 
-            transform.position = transform.position + transform.forward * m_speed * Time.deltaTime;
+            speed += acceleration * Time.deltaTime;
+            speed = Mathf.Min(speed, 10f);
 
-            GetComponent<Animator>().SetFloat("forwardSpeed", m_speed);
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
         }
+        else speed = 0;
 
-        if (Input.GetKeyUp(key))
+
+        if (GetDirectionKeyUp())
         {
-            GetComponent<Animator>().SetFloat("forwardSpeed", 0); targetDirection = transform.forward;
+            GetComponent<Animator>().SetFloat("forwardSpeed", 0);
+            targetDirection = transform.forward;
         }
+    }
+
+    private bool GetDirectionKey()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)) return true;
+        return false;
+    }
+    private bool GetDirectionKeyUp()
+    {
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S)) return true;
+        return false;
     }
 }
